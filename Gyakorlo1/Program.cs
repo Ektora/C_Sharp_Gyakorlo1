f64d888d5b41d107a898fdcedf15c403ff3ef3a0
Ektora <zsoltlucski@gmail.com>
2022-08-13 08:27:30 +0200

    Add project files.

---
+++ Gyakorlo1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            Console.Write("Enter a degree in Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(celsius + " Celsius is " + celsiusToFahrenheit(celsius) + " Fahrenheit.");
+
+        }
+
+        static double celsiusToFahrenheit(double celsius)
+        {
+            return (9.0/5) * celsius + 32;
+        }
+    }
+
+}
+
+
